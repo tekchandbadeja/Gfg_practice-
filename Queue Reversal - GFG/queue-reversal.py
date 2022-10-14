71baf7676675c71a04458038043d90173987e2bc
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-10-15 03:59:56 +0530

    Added solution - LeetHub

---
+++ Queue Reversal - GFG/queue-reversal.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+#Function to reverse the queue.
+def rev(q):
+    #add code here
+    stack=[]
+    while q.qsize()!=0:
+        stack.append(q.get())
+    while len(stack)!=0:
+        q.put(stack.pop())
+    return q
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from queue import Queue
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        q=Queue(maxsize=n+1)
+        for j in a:
+            q.put(j)
+        q=rev(q)
+        for i in range(0,n):
+            print(q.get(),end=" ")
+        print()
+# } Driver Code Ends
