3f959ae1f930adc45e83fda006ce7148391c2d3d
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-10-29 01:54:23 +0530

    Added solution - LeetHub

---
+++ Maximum Width of Tree - GFG/maximum-width-of-tree.py
@@ -0,0 +1,125 @@
+#User function Template for python3
+
+
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+def level_order_traversal(root):
+    ans=[]
+    q=[]
+    q.append(root)
+    while (len(q)!=0):
+        n=len(q)
+        curr=[]
+        for i in range (n):
+            current_node=q.pop(0)
+            curr.append(current_node.data)
+            left_child=current_node.left
+            if left_child!=None:
+                q.append(left_child)
+            right_child=current_node.right
+            if right_child!=None:
+                q.append(right_child)
+        ans.append(curr)
+    return ans 
+                
+class Solution:
+    #Function to get the maximum width of a binary tree.
+    def getMaxWidth(self,root):
+        if root==None:
+            return 0
+        ans=level_order_traversal(root)
+        count=0
+        for i in ans:
+            count=max(count,len(i))
+        return count
+       
+        #code here
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Contributed by Sudarshan Sharma
+from collections import deque
+import queue 
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        print(Solution().getMaxWidth(root))
+# } Driver Code Ends
